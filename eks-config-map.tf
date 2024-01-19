0f5442eb46c5d4fe174bbcad06d10b2766d16bfa
RjDev <rjdevnic@gmail.com>
2024-01-19 20:51:33 +0530

    disabling wiz deployment

--- eks-config-map.tf
+++ eks-config-map.tf
@@ -1,3 +1,4 @@
+?*
 resource "kubectl_manifest" "aws_auth" {
 
    yaml_body = <<YAML
@@ -15,6 +16,32 @@ data:
       - system:masters
       rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/kubectl_ssm_role
       username: system:node:{{EC2PrivateDNSName}}
+metadata:
+  labels:
+    app.kubernetes.io/managed-by: Terraform
+  name: aws-auth
+  namespace: kube-system
+
+YAML
+
+}
+*/
+resource "kubectl_manifest" "aws_auth" {
+   yaml_body = <<YAML
+
+apiVersion: v1
+data:
+  mapRoles: |
+    - groups:
+      - system:bootstrappers
+      - system:nodes
+      rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.eks_cluster_name_prefix}-node-group
+      username: system:node:{{EC2PrivateDNSName}}
+    - groups:
+      - system:masters
+      rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/kubectl_ssm_role
+      username: system:node:{{EC2PrivateDNSName}}
+kind: ConfigMap
 metadata:
   labels:
     app.kubernetes.io/managed-by: Terraform
